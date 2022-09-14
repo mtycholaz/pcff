1580c17ddb1feb424e3f13c9c2118013b66ebf49
matt.tycholaz <matthew.tycholaz@gov.ab.ca>
2022-09-14 12:05:22 -0600

    Updated prettier config formatted project files

--- apps/pcff-app/src/app/app.component.ts
+++ apps/pcff-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pcff-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'pcff-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pcff-app';
+    title = 'pcff-app';
 }
